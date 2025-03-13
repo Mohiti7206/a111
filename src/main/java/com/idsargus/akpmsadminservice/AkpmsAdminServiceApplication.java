6c3d48276648bb9dc78b161d5afcdbe1100c25e1
mohit.kumar1 <mohiti7206@gmail.com>
2025-03-13 11:28:53 +0530

    qqq

--- src/main/java/com/idsargus/akpmsadminservice/AkpmsAdminServiceApplication.java
+++ src/main/java/com/idsargus/akpmsadminservice/AkpmsAdminServiceApplication.java
@@ -15,19 +15,13 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableResourceServer
 @EnableJpaRepositories(basePackages = { "com.idsargus.akpmsadminservice.repository","com.idsargus.akpmsadminservice.Mvc.Repository" })
 @EntityScan({"com.idsargus.akpmscommonservice.entity", "com.idsargus.akpmsadminservice.entity", "com.idsargus.akpmsadminservice.Mvc.Entities"})
-//@ComponentScan(basePackages = {
-//		"com.idsargus.akpmsadminservice.Mvc.Controller"
-//})
 public class AkpmsAdminServiceApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
-
 		 SpringApplication.run(AkpmsAdminServiceApplication.class, args);
 	}
-
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
-
 }
